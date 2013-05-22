14175f3e87d2229a4f4f47b9bc5b3aa7ca9be5ca
Scott Duncombe <srduncombe@gmail.com>
2013-05-22 11:14:18 -0700

    tweets have the hash tag added to them

--- app/assets/javascripts/home_scope.js
+++ app/assets/javascripts/home_scope.js
@@ -2,19 +2,30 @@ function homePageScope($http, $scope) {
 
 	$scope.mode = 'scoreboard'
 
-	var number = 15,
+	var number = 20,
 		hashtags = '%23'+['soundoff'].join('%23')
 		query = 'https://search.twitter.com/search.json?q='+hashtags+'&rpp='+number+'&include_entities=false&result_type=recent&callback=JSON_CALLBACK'
 	$http.jsonp(query,{})
 		.success(function(data,status) {
+
 			for (var i = $scope.campaigns.length - 1; i >= 0; i--) {
-				data.results = data.results.map( function(el) { 
-					el.text = unescape(el.text)
-					el.text = el.text.replace(/\&amp;/g,'&')
-					el.created_at = new Date(el.created_at); 
-					return el; 
-				})
-				$scope.campaigns[i].tweets = data.results
+				var campaign = $scope.campaigns[i];
+
+				for (var j = 15 - 1; j >= 0; j--) {
+					var tweet = {}
+
+					for( var k in data.results[j] ) {
+						tweet[k] = data.results[j][k]
+					}
+					
+					tweet.text = unescape( tweet.text)
+					tweet.text = tweet.text.replace(/\&amp;/g,'&')
+					tweet.text = tweet.text.replace(/\#soundoff/gi,campaign.name.replace(/\s/g,'')+' #SoundOff')
+					tweet.created_at = new Date(tweet.created_at); 
+
+
+					$scope.campaigns[i].tweets.push( tweet )
+				};
 			};
 		})
 	
@@ -44,5 +55,7 @@ function homePageScope($http, $scope) {
 			tweets: []
 		}
 		
-	]
+	].sort( function(a,b) { 
+		return a.score > b.score ? -1 : 1
+	})
 }
