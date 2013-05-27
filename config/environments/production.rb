baaba6f4f5f3ecb7c1f0c27d9ed63dc70148148b
Scott Duncombe <srduncombe@gmail.com>
2013-05-27 13:33:44 -0700

    make widget available

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ SoundOff::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
   
-  config.assets.precompile += %w(angular.min.js widget_snippet.js)
+  config.assets.precompile += %w(angular.min.js widget_snippet.js widget.js)
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
