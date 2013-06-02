c6f4e7a1cc18445ba04acfbc76ef1654eee29391
Scott Duncombe <srduncombe@gmail.com>
2013-06-02 02:31:03 -0700

    another typo - bad heroku

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,5 +1,5 @@
 class Partner < ActiveRecord::Base
-  attr_accessible :name, :website, :tax_id, :logo, :partner_type
+  attr_accessible :name, :website, :tax_id, :logo, :partner_type,
   	:contact_email, :contact_phone, :contact_name,
 
   has_many :users
