7d31549bd76576ba9a8ef00107fcae6c6e6b051b
Scott Duncombe <srduncombe@gmail.com>
2013-06-03 23:23:34 -0700

    final og tweaks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,12 +8,11 @@ class HomeController < ActionController::Base
   		rep.data = nil
       rep[:short_url] = rep_path( rep.twitter_screen_name )
 
-      @title = rep.name
+      @title = '@'+rep.name
       @og_title = @title+' | #SoundOff @ Congress'
-      @og_description = "#SoundOff @#{rep.twitter_screen_name} (#{rep.name} from #{rep.state_name}) and see what others are saying. #SoundOff is an advocacy tool created and maintained by HeadCount.org."
-      @og_image = 'https://api.twitter.com/1/users/profile_image?screen_name='+rep.screen_name
+      @og_description = "#SoundOff @ #{rep.name} from #{rep.state_name} and see what others are saying. #SoundOff is an advocacy tool created and maintained by HeadCount.org."
+      @og_image = 'https://api.twitter.com/1/users/profile_image?screen_name='+rep.twitter_screen_name
       
-      @title = [rep.title,rep.first_name,rep.last_name,'|','#SoundOff @ Congress'].join(' ') 
   	end
 
     # Will want to do something here with campaigns
