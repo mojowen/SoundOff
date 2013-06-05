1127722eb1f3b748bf1226a60aab4437af0e40cb
Scott Duncombe <srduncombe@gmail.com>
2013-06-05 16:35:21 -0700

    white space fixes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ActionController::Base
   layout 'application.html'
-  
+
   def home
-  	
+
   	if params[:twitter_screen_name]
   		rep = Rep.find_by_twitter_screen_name( params[:twitter_screen_name] )
   		rep.data = nil
@@ -12,13 +12,13 @@ class HomeController < ActionController::Base
       @og_title = @title+' | #SoundOff @ Congress'
       @og_description = "#SoundOff @ #{rep.name} from #{rep.state_name} and see what others are saying. #SoundOff is an advocacy tool created and maintained by HeadCount.org."
       @og_image = 'https://api.twitter.com/1/users/profile_image?screen_name='+rep.twitter_screen_name
-      
+
   	end
 
     # Will want to do something here with campaigns
 
-    @config = { 
-    	:home => true, 
+    @config = {
+    	:home => true,
     	:single => params[:short_url] || rep || nil,
       :random_reps => Rep.all( :order => 'RANDOM()', :limit => 10 ).map{ |r| r.data = nil; r }
    	}
