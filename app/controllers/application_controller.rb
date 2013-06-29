0c8d2c4f5e61872a2a74197d4bf70f4b5ab00710
Scott Duncombe <srduncombe@gmail.com>
2013-06-28 19:28:42 -0700

    redirct to sign in if not signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def only_logged_in
-  	redirect_to home_path if !!! current_user
+  	redirect_to new_user_session_path if !!! current_user
   end
 
   def after_sign_in_path_for(resource)
