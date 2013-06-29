8696ffddcd6b92ed2d69e94e41b3e750c85e0325
Scott Duncombe <srduncombe@gmail.com>
2013-06-28 19:20:59 -0700

    fixing up top menu on touch devices

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,15 +23,15 @@ if (!Array.prototype.filter) {
   Array.prototype.filter = function(fun /*, thisp*/)
   {
     "use strict";
- 
+
     if (this == null)
       throw new TypeError();
- 
+
     var t = Object(this);
     var len = t.length >>> 0;
     if (typeof fun != "function")
       throw new TypeError();
- 
+
     var res = [];
     var thisp = arguments[1];
     for (var i = 0; i < len; i++)
@@ -43,12 +43,25 @@ if (!Array.prototype.filter) {
           res.push(val);
       }
     }
- 
+
     return res;
   };
 }
 var months = ['Jan', 'Feb', 'March', 'April', 'May', 'June','July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-if( isMobile.any() ) $(document).on('click touchstart','#top_menu a.menu',function() {f
-    var $this = $(this)
-    if( $this.next('.inner').is(':visible') ) $this.blur()
+function is_touch_device() {
+  return !!('ontouchstart' in window) // works on most browsers
+      || !!('onmsgesturechange' in window); // works on ie10
+};
+if( is_touch_device() ) {
+  document.body.classList.add('touch');
+}
+$(document).on('click touchstart','#top_menu a.menu',function(e) {
+  if( is_touch_device() ) {
+    var $parent = $(this).parent()
+    if( ! $parent.hasClass('on') ) $('#main').bind('click touchstart', function() {  $parent.removeClass('on'); $(this).unbind('click touchstart') });
+    else $('#main').unbind('click touchstart')
+
+    $parent.toggleClass('on')
+  }
+  e.preventDefault();
 })
