61859c4433974b646237fd8f21c23904023311d0
Scott Duncombe <srduncombe@gmail.com>
2013-07-05 02:38:49 -0700

    secret token should not be in public repo

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SoundOff::Application.config.secret_token = '06a955419ede82a5253e73ddec34626f95460f84697588f68f46a9798d2c886dbee8355f7afafa134d734184a9903c88e14f66676b1ff130c4fab81262178b70'
+SoundOff::Application.config.secret_token = ENV['SECRET'] || '06a955419ede82a5253e73ddec34626f95460f84697588f68f46a9798d2c886dbee8355f7afafa134d734184a9903c88e14f66676b1ff130c4fab81262178b70'
