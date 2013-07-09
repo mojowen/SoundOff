fce0b20e34a6fce5c43bb085b958aa5c60583981
Scott Duncombe <srduncombe@gmail.com>
2013-07-09 10:52:54 -0700

    don't clear congress if fixed

--- app/assets/javascripts/home_scope.js
+++ app/assets/javascripts/home_scope.js
@@ -196,7 +196,7 @@ function homePageScope($http, $scope) {
 		if( w_width > med_cut_off ) $scope.active = $scope.items()[0]
 
 		history.pushState( {campaign: $scope.items[0] }, 'Sound Off',  '/' );
-		$(logo).attr('src','/assets/SoundOffWhiteBeta.svg')
+		if( document.body.classList.contains('fixed') ) $(logo).attr('src','/assets/SoundOffWhiteBeta.svg')
 		document.title = '#SoundOff @ Congress'
 	}
 	$scope.resetHard = function() {
