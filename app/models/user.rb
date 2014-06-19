e68e237665dce163de5a332a528cb906dd9fcbb1
Scott Duncombe <srduncombe@gmail.com>
2014-06-19 09:40:47 -0700

    remove token_authenticable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
 
   devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :token_authenticatable
+         :recoverable, :rememberable, :trackable, :validatable
 
   attr_accessible :email, :password, :password_confirmation, :remember_me,
     :name, :phone, :partner
