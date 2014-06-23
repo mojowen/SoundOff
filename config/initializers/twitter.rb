00b6b0c48e5d71b0ffe8dc73ee2125a92099af93
mojowen <srduncombe@gmail.com>
2014-06-23 16:43:05 -0700

    use better twitter labels

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,6 +1,6 @@
 twitter_client = Twitter::REST::Client.new do |config|
   config.consumer_key       = ENV['TWITTER_CONSUMER_KEY']
   config.consumer_secret    = ENV['TWITTER_CONSUMER_SECRET']
-  config.oauth_token        = ENV['TWITTER_OAUTH_TOKEN']
-  config.oauth_token_secret = ENV['TWITTER_OATH_SECRET']
+  config.access_token        = ENV['TWITTER_OAUTH_TOKEN']
+  config.access_token_secret = ENV['TWITTER_OATH_SECRET']
 end
