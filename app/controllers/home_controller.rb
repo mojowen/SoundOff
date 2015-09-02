caf92899e7f3ae7b549c0cc2dc25647340982067
mojowen <srduncombe@gmail.com>
2015-09-02 15:26:41 -0700

    small cleanup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -155,14 +155,14 @@ class HomeController < ApplicationController
         partner = Partner.find_by_twitter_screen_name(params[:twitter_screen_name].gsub('@',''))
         partner.save() if partner
       else
-        user = TWITTER_CLIENT.user(params[:twitter_screen_name].gsub('@',''))
-        rep = Rep.find_by_twitter_screen_name(user.screen_name)
+        user = TWITTER_CLIENT.user('whitehouse')params[:twitter_screen_name].gsub('@',''))
+        rep = Rep.find_by_twitter_screen_name(user.screen_name.downcase)
         rep.update_attributes( :twitter_profile_image => user.profile_image_url.to_s ) if rep
       end
       redirect_to partner ? partner.logo : rep.twitter_profile_image
     rescue  => e
       puts "Avatar fallback failed with #{e}"
-      redirect_to "http://a0.twimg.com/sticky/default_profile_images/default_profile_6_bigger.png"
+      redirect_to 'https://abs.twimg.com/sticky/default_profile_images/default_profile_6.png'
     end
   end
 
