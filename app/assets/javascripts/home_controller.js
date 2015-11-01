f8929a7df1a319e6453c255f4e7412c9c249984a
mojowen <srduncombe@gmail.com>
2015-11-01 13:49:13 -0800

    only fetch single hashtag

--- app/assets/javascripts/home_controller.js
+++ app/assets/javascripts/home_controller.js
@@ -138,7 +138,7 @@ function homePageScope($http, $scope) {
 		if( typeof $scope.active != 'undefined' ) {
 			var active_list = $scope.mode.toLowerCase() == 'reps' ? $scope.reps() : $scope.campaigns(),
 				position = active_list.indexOf( $scope.active )
-			loadActive( active_list.slice(position, position + 3) )
+			loadActive( active_list.slice(position, position + 1) )
 		}
 	})
 
